7d256c04be39f567762b46bd381ac4ecec4855d2
Mahdi Alikhani2005 <mahdialikhaniapr2005@gmail.com>
2024-06-22 08:29:09 +0330

    Make-sign-in

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -4,6 +4,20 @@ import {
   getFromLocalStorage,
 } from "../../utils/storageUtils.js";
 import { createElementRepo } from "../../utils/domUtils.js";
-const getDataFromStorage = getFromLocalStorage("userData", [0]);
+const getDataFromStorage = getFromLocalStorage("userData");
+// const saveData = saveToLocalStorage();
+const inputs = document.querySelectorAll("input");
+window.onload = function () {
+  inputs[0].value = "";
+  inputs[1].value = "";
+};
+
 console.log(getDataFromStorage);
-const saveData = saveToLocalStorage();
+const formSignIn = document.querySelector("form");
+formSignIn.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const admins = getDataFromStorage.filter((user) => {
+    return user.Email === "mahdialikhaniapr2005@gmail.com" || "Aghil@gmail.com";
+  });
+  console.log(admins);
+});
